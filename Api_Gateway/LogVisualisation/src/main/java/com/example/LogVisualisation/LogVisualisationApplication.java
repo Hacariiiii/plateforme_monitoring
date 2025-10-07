2ed2c8d59235d1cb037851f14b1805e2df12a311
hacariiiii <hebamouad1234@gmail.com>
2025-10-08 01:32:48 +0200

    first commit

---
+++ Api_Gateway/LogVisualisation/src/main/java/com/example/LogVisualisation/LogVisualisationApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogVisualisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogVisualisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogVisualisationApplication.class, args);
+	}
+
+}
