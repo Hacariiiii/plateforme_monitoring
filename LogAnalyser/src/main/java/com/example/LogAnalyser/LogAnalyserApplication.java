16d5ab83df7853eeb10136f6f3024ae782d20f92
atifHiba <atifwahiba1@gmail.com>
2025-11-24 15:16:33 +0100

    Ajout Eureka Server et API Gateway

---
+++ LogAnalyser/src/main/java/com/example/LogAnalyser/LogAnalyserApplication.java
@@ -0,0 +1,15 @@
+package com.example.LogAnalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LogAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyserApplication.class, args);
+	}
+
+}
---
+++ LogCollector/src/main/java/com/example/LogCollector/LogCollectorApplication.java
@@ -0,0 +1,17 @@
+package com.example.LogCollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LogCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogCollectorApplication.class, args);
+    }
+
+
+}
+
